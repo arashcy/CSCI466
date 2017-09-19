bbfb4137ee728e9f97cd73723eaaf0f35b0527b6
Elizabeth Andrews <ecandrews0@icloud.com>
2017-09-19 12:59:00 -0600

    initial commit

---
+++ CSCI466 - Program1/client.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+#Brock Ellefson Lizzie Andrews
+
+import socket
+import sys
+#import httplib
+import re
+#----------------------------------------------------
+#arguments we pass in
+ip = sys.argv[1]
+port = int(sys.argv[2])
+x = sys.argv[3]
+y = sys.argv[4]
+#----------------------------------------------------
+
+def decode(data):
+        match = re.search("hit=\d", data)
+        hit = match.group(0) if match else None
+        return (hit)
+
+def main():
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #creates socket
+    s.connect((ip, port)) #connects us to server
+    print ("Connected")
+
+    #headers
+    coordinates = ("x=" + x + "&y=" + y)
+    conntype = 'Connection: close'
+    conttype = 'Content-Type: application/x-uuu-form-urlencoded'
+    user = 'User-Agent: client.py'
+    contlen = 'Content-Length:  %s' %(len(coordinates))
+
+
+    s.send(bytes("POST / HTTP/1.1\n%s\n%s\n%s\n%s\n%s" %(conntype, conttype, user, contlen, coordinates) )) #post to server
+    data = s.recv(1024) #decode data we get back from server
+    print (data)
+
+
+    s.close() #close connection
+    if 'hit' in data:
+        hit = decode(data)
+        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #creates socket
+        coordinates += hit
+        contlen = 'Content-Length:  %s' %(len(coordinates))
+        s.connect(('192.168.1.12', 5000))
+        s.send(bytes("POST / HTTP/1.1\n%s\n%s\n%s\n%s\n%s" %(conntype, conttype, user, contlen, coordinates) )) #post to server
+        s.close()
+main()
